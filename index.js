a7138caa256095e8f8737aaf823842730c9033b2
fernando <pcabral.fernando@gmail.com>
2022-06-30 20:33:33 -0300

    mudança da variavel de ambiente upside down

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
-const { UPSIDEDOWN_MODE } = process.env.UPSIDEDOWN_MODE;
+const { UPSIDEDOWN_MODE } = process.env;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
